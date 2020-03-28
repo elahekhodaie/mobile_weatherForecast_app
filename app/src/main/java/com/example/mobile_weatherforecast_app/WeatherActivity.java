3962227cd5e60062654c1aec75d108d879cc68c8
ellykhodaie <elahekhodaie@yahoo.com>
2020-03-28 11:57:10 +0430

    Weather activity class is added

---
+++ app/src/main/java/com/example/mobile_weatherforecast_app/WeatherActivity.java
@@ -0,0 +1,60 @@
+package com.example.mobile_weatherforecast_app;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.graphics.Typeface;
+import android.graphics.drawable.Drawable;
+import android.media.Image;
+import android.os.Handler;
+import android.os.StrictMode;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import org.json.JSONArray;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
+import java.util.Calendar;
+
+import org.json.JSONObject;
+
+import android.content.Context;
+import android.util.Log;
+
+
+//better naming scheme
+public class WeatherActivity extends Activity {
+
+    private static final String API = " ";
+
+    Typeface weatherFont;
+    TextView cityField;
+    TextView updatedField;
+    TextView detailsField;
+    TextView currentTemperatureField;
+    TextView weatherIcon;
+    int backgroundImageId = 0;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+       
+
+
+    }
+
+}
